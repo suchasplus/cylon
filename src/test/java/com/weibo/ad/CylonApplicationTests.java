7d447960b650ddb64f463fb49e70ae606bc5ebde
suchasplus <suchasplus@gmail.com>
2017-01-04 14:45:42 +0800

    initial

---
+++ src/test/java/com/weibo/ad/CylonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weibo.ad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CylonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
